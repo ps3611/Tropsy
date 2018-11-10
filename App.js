525fb46557a6b269a56c3a9579096519a152f7f0
Peter Schindler <petertasschindler@gmail.com>
2018-11-10 17:37:08 +0000

    refactor: flex layout

--- App.js
+++ App.js
@@ -1,35 +1,27 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, ScrollView } from 'react-native';
 import Row from './Row.js';
 import data from './atp-rankings.json';
-
-// const data = [
-//   { name: 'Novak Djokovic', nationality: 'SRB', ranking: 1, age: 31, points: 4000 },
-//   { name: 'Novak Djokovic', nationality: 'SRB', ranking: 1, age: 31, points: 4000 },
-//   { name: 'Novak Djokovic', nationality: 'SRB', ranking: 1, age: 31, points: 4000 }
-// ];
 
 export default class App extends React.Component {
   render() {
     const rows = data.map((el,i) => {
       return <Row
         key={i}
-        value={el.fields.player_name}
+        currentRank={el.fields.current_rank}
+        playerName={el.fields.player_name}
+        playerCountry={el.fields.player_country}
+        playerPoints={el.fields.player_points}
         />
     })
     return (
-      <View style={styles.container}>
+        <ScrollView>
         {rows}
-      </View>
+        </ScrollView>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+
 });
--- Row.js
+++ Row.js
@@ -1,12 +1,22 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default class Row extends React.Component {
   render() {
     return (
-      <View>
-        <Text>{this.props.value}</Text>
+      <View style={styles.row}>
+        <Text>{this.props.currentRank}</Text>
+        <Text>{this.props.playerName}</Text>
+        <Text>{this.props.playerCountry}</Text>
+        <Text>{this.props.playerPoints}</Text>
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  row: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+});
