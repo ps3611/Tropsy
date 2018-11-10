eeeb1b8635ebcdea09b97bbcb08880830cca3b23
Peter Schindler <petertasschindler@gmail.com>
2018-11-10 14:57:07 +0000

    refactor: all

--- App.js
+++ App.js
@@ -6,7 +6,6 @@ export default class App extends React.Component {
     return (
       <View style={styles.container}>
         <Text>Open up App.js to start working on your app!</Text>
-        <Text>Open up App.js to start working on your app!</Text>
       </View>
     );
   }
