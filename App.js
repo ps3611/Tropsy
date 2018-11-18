e9459fbe71e8595bb2da447eb8b9a330d120fe2d
Peter Schindler <petertasschindler@gmail.com>
2018-11-18 14:27:35 +0100

    refactor: create views folder

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Provider } from 'react-redux';
 
 import store from './store.js';
-import Rankings from './Rankings.js';
+import Rankings from './views/Rankings.js';
 
 export default class App extends React.Component {
     render() {
