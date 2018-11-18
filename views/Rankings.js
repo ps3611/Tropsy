5a94304bc3db5a5c3a05e23a808486e203496bf4
Peter Schindler <petertasschindler@gmail.com>
2018-11-18 15:23:20 +0100

    feat: changing data to endpoint data

--- views/Rankings.js
+++ views/Rankings.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import { View, ScrollView } from 'react-native';
+import { connect } from 'react-redux';
+import { fetchPlayerList } from '../actions/apiActions.js'
 import Navbar from './Navbar.js';
 import Row from './Row.js';
-import data from './data.json';
 
 /* eslint-disable react/prefer-stateless-function, react/jsx-filename-extension, react/prop-types */
 
+class Rankings extends React.Component {
 
-export default class Rankings extends React.Component {
+  componentDidMount() {
+    this.props.fetchPlayerList();
+  }
+
   render() {
+    const { data } = this.props;
     const rows = data.map((el, i) => {
       return (
         <Row
@@ -39,3 +45,13 @@ export default class Rankings extends React.Component {
     );
   }
 }
+
+const mapStateToProps = state => ({
+  data: state.api.playerList,
+});
+
+const mapDispatchToProps = dispatch => ({
+  fetchPlayerList: () => dispatch(fetchPlayerList()),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Rankings);
