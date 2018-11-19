47d0777dc451081f76297667b5ff1a1ddd8966e4
Peter Schindler <petertasschindler@gmail.com>
2018-11-20 00:08:01 +0100

    feat: add group button

--- views/Navbar.js
+++ views/Navbar.js
@@ -1,20 +1,38 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import { Icon } from 'react-native-elements';
+import { Icon, ButtonGroup } from 'react-native-elements';
 
 
 /* eslint-disable react/prefer-stateless-function, react/jsx-filename-extension, react/prop-types */
 
 
 export default class Navbar extends React.Component {
+  constructor() {
+    super();
+    this.state = { selectedIndex: 2 };
+    this.updateIndex = this.updateIndex.bind(this);
+  }
+
+  updateIndex(selectedIndex) {
+    this.setState({ selectedIndex });
+  }
+
   render() {
+    //https://react-native-training.github.io/react-native-elements/docs/button_group.html
+    const buttons = ['ATP', 'ELO'];
+    const { selectedIndex } = this.state;
     return (
       <View style={styles.navbar}>
         <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
           <Icon name="menu" />
         </View>
         <View style={{ flex: 4, justifyContent: 'center', alignItems: 'center' }}>
-          <Text>Tennis Roo</Text>
+          <ButtonGroup
+            onPress={this.updateIndex}
+            selectedIndex={selectedIndex}
+            buttons={buttons}
+            containerStyle={{ margin: 15, backgroundColor: 'red', borderRadius: 10 }}
+          />
         </View>
         <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
           <Icon name="info-outline" />
