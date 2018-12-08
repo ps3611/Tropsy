c735526249c69e24c9f9fa4b0c4471c51ddd7bb0
Peter Schindler <petertasschindler@gmail.com>
2018-12-07 18:10:04 -0800

    fix: minor tweeks

--- actions/apiActions.js
+++ actions/apiActions.js
@@ -1,6 +1,7 @@
 import { RSAA } from 'redux-api-middleware';
 
 const baseUrl = 'http://127.0.0.1:8000';
+// const baseUrl = 'http://192.168.1.23:8000';
 
 export const PLAYER_LIST_RESQUEST = '@@api/PLAYER_LIST_RESQUEST';
 export const PLAYER_LIST_SUCCESS = '@@api/PLAYER_LIST_SUCCESS';
