3cad1cb9192fadcb61c2b33a1bd3df3855e8dbf6
Peter Schindler <petertasschindler@gmail.com>
2018-12-14 15:51:51 +0100

    chore: link to AWS

--- actions/apiActions.js
+++ actions/apiActions.js
@@ -1,7 +1,8 @@
 import { RSAA } from 'redux-api-middleware';
 
-const baseUrl = 'http://127.0.0.1:8000';
-// const baseUrl = 'http://192.168.1.23:8000';
+const baseUrl = 'http://34.201.104.13:8000'; // AWS
+// const baseUrl = 'http://127.0.0.1:8000'; // desktop mobile
+// const baseUrl = 'http://192.168.1.23:8000'; // mobile
 
 export const PLAYER_LIST_RESQUEST = '@@api/PLAYER_LIST_RESQUEST';
 export const PLAYER_LIST_SUCCESS = '@@api/PLAYER_LIST_SUCCESS';
