5aa77328e8e90843cf0debba3bee6ebf807f33be
Peter Schindler <petertasschindler@gmail.com>
2018-12-18 10:57:21 +0100

    refactor: extract Rankings style2

---
+++ styles/Rankings.js
@@ -0,0 +1,14 @@
+import { MediaQueryStyleSheet } from 'react-native-responsive';
+
+module.exports = MediaQueryStyleSheet.create(
+  {
+    rankingsView: {
+      flex: 1,
+    },
+    loadingView: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+  },
+);
