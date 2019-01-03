3c1babe9ab35bf9ffcf967b135416ecb36b1c79a
Peter Schindler <petertasschindler@gmail.com>
2019-01-03 14:54:48 +0100

    refactor: add .env

--- src/server/tropsy/settings.py
+++ src/server/tropsy/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import environ
+env = environ.Env()
+environ.Env.read_env()
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '%a%l1o+(%%bxg8i6@b9$@e4uhgo50ekcky0#p#i1$xrik)$*m0'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -77,12 +80,12 @@ WSGI_APPLICATION = 'tropsy.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'HOST': 'tennisroo.cd16zrysaphf.eu-west-2.rds.amazonaws.com',
-        'NAME': 'tennisroo',
-        'USER': 'master_user',
-        'PASSWORD': 'tennisroo',
-        'PORT': '5432',
+        'ENGINE': env('DB_ENGINE'),
+        'HOST': env('DB_HOST'),
+        'NAME': env('DB_NAME'),
+        'USER': env('DB_USER'),
+        'PASSWORD': env('DB_PASSWORD'),
+        'PORT': env('DB_PORT'),
     },
 }
 
